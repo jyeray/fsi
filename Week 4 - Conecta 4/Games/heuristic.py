67ce09816b72dbdc5bcb82fd26583665b71c2616
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-04-22 17:12:52 +0100

    Improve horizontal heuristic

--- Week 4 - Conecta 4/Games/heuristic.py
+++ Week 4 - Conecta 4/Games/heuristic.py
@@ -3,20 +3,46 @@ from random import randint
 def randomHeuristic(state):
     return randint(0, 100)
 
+
 def horizontalHeuristic(state):
     result = 0
-    for point in state.board:
-        if state.board[point] == 'X':
-            result += horizontalCount(state, point)
+    for number in range(1, 7):
+        line = {}
+        for point in state.board:
+            if point[1] == number:
+                line[point] = state.board[point]
+        result += horizontalLineHeuristic(line, state.moves)
     return result
 
 
-def horizontalCount(state, point):
-    player = state.board[point]
+def getHorizontalPoints(moves, point, inrow):
+    multiplier = 0
+    if inrow == 1:
+        return 0
+    leftpoint = (point[0] - 1, point[1])
+    rightpoint = (point[0] + inrow, point[1])
+    if leftpoint in moves:
+        multiplier += 20
+    if rightpoint in moves:
+        multiplier += 20
+    return inrow * multiplier
+
+
+def horizontalLineHeuristic(line, moves):
+    result = 0
+    for point in line:
+        if line[point] == 'X':
+            inrow = horizontalCount(line, point)
+            result += getHorizontalPoints(moves, point, inrow)
+    return result
+
+
+def horizontalCount(line, point):
+    player = line[point]
     count = 1
     point = (point[0] + 1, point[1])
-    while point in state.board:
-        if state.board[point] == player:
+    while point in line:
+        if line[point] == player:
             count += 1
             point = (point[0] + 1, point[1])
         else:
