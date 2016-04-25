6926990cd55ab2e45859f0a7757008afbff8d5d5
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-04-25 19:51:33 +0100

    Add inverse diagonal heuristic

---
+++ Week 4 - Conecta 4/Games/InverseDiagonalHeuristic.py
@@ -0,0 +1,49 @@
+def inverseDiagonalHeuristic(state):
+    initial_points = {(1, 4), (1, 5), (1, 6), (2, 6), (3, 6), (4, 6)}
+    result = 0
+    for point in initial_points:
+        result += diagonalLineHeuristic(point, state.board, state.moves)
+    return result
+
+
+def diagonalLineHeuristic(point, board, moves):
+    result = 0
+    while point in board or point in moves:
+        if point in board:
+            inrow = diagonalCount(point, board)
+            if board[point] == 'X':
+                result += getDiagonalPoints(point, inrow, moves)
+            else:
+                result -= getDiagonalPoints(point, inrow, moves)
+            point = (point[0] + inrow, point[1] - inrow)
+        else:
+            point = (point[0] + 1, point[1] - 1)
+    return result
+
+
+def diagonalCount(point, board):
+    player = board[point]
+    point = (point[0] + 1, point[1] - 1)
+    count = 1
+    while point in board:
+        if board[point] == player:
+            count += 1
+            point = (point[0] + 1, point[1] - 1)
+        else:
+            return count
+    return count
+
+
+def getDiagonalPoints(point, inrow, moves):
+    multiplier = 0
+    if inrow == 1:
+        return 0
+    if inrow == 4:
+        return float('inf')
+    down_point = (point[0] + inrow, point[1] - inrow)
+    up_point = (point[0] - 1, point[1] + 1)
+    if up_point in moves:
+        multiplier += 2
+    if down_point in moves:
+        multiplier += 2
+    return (inrow * multiplier) ** inrow
--- Week 4 - Conecta 4/Games/heuristic.py
+++ Week 4 - Conecta 4/Games/heuristic.py
@@ -2,6 +2,7 @@ from random import randint
 from HorizontalHeuristic import horizontalHeuristic
 from VerticalHeuristic import verticalHeuristic
 from DiagonalHeuristic import diagonalHeuristic
+from InverseDiagonalHeuristic import inverseDiagonalHeuristic
 
 
 def randomHeuristic(state):
@@ -13,4 +14,5 @@ def combinedHeuristic(state):
     result += horizontalHeuristic(state)
     result += verticalHeuristic(state)
     result += diagonalHeuristic(state)
+    result += inverseDiagonalHeuristic(state)
     return result
